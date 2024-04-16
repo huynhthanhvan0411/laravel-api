120c9c0d7b16040a86c61321a1e9161cc4615610
thanhvan <huynhthanhvan0411@gmail.com>
2024-04-16 13:47:25 +0700

    r

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ExampleController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/page', 'ExampleController@show')->name('page.show');
