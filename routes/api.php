fe9af78aed928948aafda1bbc3f623e60db941c2
thanhvan <huynhthanhvan0411@gmail.com>
2024-04-17 13:48:54 +0700

    done

--- routes/api.php
+++ routes/api.php
@@ -16,24 +16,31 @@ use App\Http\Controllers\API\AuthController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-Route::get('/products',[ProductController::class, 'index']);
-Route::post('/products',[ProductController::class, 'store']);
-Route::get('/products/{id}',[ProductController::class, 'show']);
-Route::put('/products/{id}',[ProductController::class, 'update']);
-Route::delete('/products/{id}',[ProductController::class, 'destroy']);
-Route::delete('/products/search/name',[ProductController::class, 'searchName']);
+
 
 //API route để đăng ký
 Route::post('/register', [App\Http\Controllers\API\AuthController::class, 'register']);
 //API route để đăng nhập
 Route::post('/login', [AuthController::class, 'login']);
+
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    //API route danh sách sản phẩm
+    Route::get('/products',[ProductController::class, 'index']);
+    Route::post('/products',[ProductController::class, 'store']);
+    Route::get('/products/{id}',[ProductController::class, 'show']);
+    Route::put('/products/{id}',[ProductController::class, 'update']);
+    Route::delete('/products/{id}',[ProductController::class, 'destroy']);
+    Route::delete('/products/search/{name}',[ProductController::class, 'searchName']);
+    // API route thoát
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::get('/profile', function(Request $request) {
         return auth()->user();
     });
-    // API route thoát
-    Route::post('/logout', [AuthController::class, 'logout']);
-});
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+
 });
